de54d1a4edfce0060dd36166f716301948d29d45
bold_max <munkhbold260@gmail.com>
2024-03-15 00:11:56 +0800

    fix

--- src/components/records/left/Left.jsx
+++ src/components/records/left/Left.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
@@ -8,6 +9,9 @@ import AddButton from "@/components/logos/AddButton";
 import Category from "./Category";
 import Types from "./Types";
 import XButton from "@/components/logos/XButton";
+import { Input } from "./Input";
+import { Option, Option4 } from "./Option";
+import { Daypicker, Timepicker } from "./Daypicker";
 
 const style = {
   position: "absolute",
@@ -22,9 +26,10 @@ const style = {
 };
 
 export default function Left() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [color, setColor] = useState("blue");
 
   return (
     <div className="w-[282px] h-[1080px] px-4 py-6 bg-gray-50 rounded-xl border border-gray-200 flex-col justify-start items-start gap-10 inline-flex">
@@ -46,13 +51,102 @@ export default function Left() {
               aria-labelledby="modal-modal-title"
               aria-describedby="modal-modal-description"
             >
-              <Box className=" w-full h-full flex justify-center items-center">
+              <Box className=" w-full h-full flex flex-col justify-center items-center">
                 <div className="rounded-xl bg-red-300">
                   <div className="w-[792px] px-6 py-5  border-b border-slate-200 justify-between items-center inline-flex">
                     <div className="text-slate-900 text-xl font-semibold  leading-7">
                       Add Record
                     </div>
                     <XButton />
+                  </div>
+                </div>
+                ndfkjsdjk
+                <div className="border-t border-[#E2E8F0] flex justify-center ">
+                  <div className="py-5 px-6 w-[396px] gap-4 flex flex-col">
+                    <div className="flex bg-[#F3F4F6] rounded-3xl">
+                      <button
+                        className={` rounded-3xl  w-[172px] h-[40px] ${
+                          color == "blue"
+                            ? "bg-[#0166FF] text-[#F9FAFB] "
+                            : "bg-[#F3F4F6]  text-[#1F2937]"
+                        }`}
+                        onClick={() => {
+                          setColor("blue");
+                        }}
+                      >
+                        Expense
+                      </button>
+                      <button
+                        className={` rounded-3xl w-[172px] h-[40px] ${
+                          color == "green"
+                            ? "bg-[#16A34A] text-[#F9FAFB]"
+                            : "bg-[#F3F4F6] text-[#1F2937]"
+                        }`}
+                        onClick={() => {
+                          setColor("green");
+                        }}
+                      >
+                        Income
+                      </button>
+                    </div>
+                    <div className="flex flex-col gap-5 ">
+                      <div className="bg-[#F3F4F6] rounded-2xl w-full px-2 flex flex-col justify-center h-[76px]">
+                        <p className="text-base font-normal not-italic px-6 ">
+                          Amount
+                        </p>
+                        <Input />
+                      </div>
+
+                      <div className="">
+                        <p className="text-base not-italic font-normal p-1 ">
+                          Category
+                        </p>
+                        <div className="relative ">
+                          <Option />
+                        </div>
+                      </div>
+                      <div className="flex justify-between">
+                        <div className="w-[168px]">
+                          <p className="text-base not-italic font-normal p-1">
+                            Date
+                          </p>
+                          <Daypicker />
+                        </div>
+                        <div className="w-[168px]">
+                          <p className="text-base not-italic font-normal p-1">
+                            Time
+                          </p>
+                          <Timepicker />
+                        </div>
+                      </div>
+                      <button
+                        onClick={() => {
+                          setColor("blue");
+                        }}
+                        className={`btn h-8  rounded-3xl ${
+                          color == "blue"
+                            ? "bg-[#0166FF] text-[#FFFFFF] "
+                            : "bg-[#16A34A] text-[#F9FAFB]"
+                        }`}
+                      >
+                        Add Record
+                      </button>
+                    </div>
+                  </div>
+                  <div className="pt- pb-6 px-6 w-[396px] flex flex-col gap-8">
+                    <div>
+                      <p className="text-base not-italic font-normal p-1">
+                        Payee
+                      </p>
+                      <Option4 />
+                    </div>
+                    <div>
+                      <p>Note</p>
+                      <textarea
+                        placeholder="Write here"
+                        className="textarea textarea-bordered border-none textarea-lg w-full min-h-[280px] max-w-xs"
+                      ></textarea>
+                    </div>
                   </div>
                 </div>
               </Box>
